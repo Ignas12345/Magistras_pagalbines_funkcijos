dcd1dfed11dcc94305b179e83b00427e0364beb4
Ignas12345 <155150364+Ignas12345@users.noreply.github.com>
2025-03-13 16:18:43 +0200

    prideta projektavimo funkcija

--- projektavimo metodai.py
+++ projektavimo metodai.py
@@ -1,10 +1,16 @@
+import numpy as np
+import pandas as pd
+from sklearn.decomposition import PCA, FastICA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.preprocessing import StandardScaler
+
 def perform_method(model, data_df: str | np.ndarray | None = None, supervised = False, sample_label_dict: dict | None = None, data_array: np.ndarray | None = None,
-                              sample_ids=None, feature_ids=None, transpose_df=False, scale_data: bool = False, with_std = True,
+                              sample_ids=None, feature_ids=None, transpose_df=False, center_data: bool = True, scale_data: bool = False,
                               return_model=False, outliers_to_filter = None):
     """
     Performs dimensionality reduction using the provided model.
     The input data should have observations per row and features per column.
-    Scale data can either be True/False (if True Standard scaler is used) or a function that scales the data, such as mean subtraction - the function always
+    Scale data can either be True/False. If the data is not centered, then it will also not be scaled.
     """
     if data_df is not None:
         if isinstance(data_df, str):
@@ -17,8 +23,12 @@ def perform_method(model, data_df: str | np.ndarray | None = None, supervised = 
         feature_ids = data_df.columns
         sample_ids = data_df.index
 
-    if scale_data:
-        data_array = StandardScaler(with_std=with_std).fit_transform(data_array)
+    if scale_data and center_data:
+        data_array = StandardScaler().fit_transform(data_array)
+    elif center_data:
+        data_array = StandardScaler(with_std=False).fit_transform(data_array)
+    elif scale_data:
+        raise ValueError("data should not be scaled, unless first centered (set center_data to True)")
 
     if outliers_to_filter is not None:
         mask = [sample not in outliers_to_filter for sample in sample_ids]
@@ -55,17 +65,4 @@ def perform_method(model, data_df: str | np.ndarray | None = None, supervised = 
     print(message_return_tuple + ')')
     return tuple_to_return
 
-def filter_samples_from_df(df, samples_to_filter, sample_ordering = None):
-  if samples_to_filter is None:
-    return df
-  sample_names = df.index
-  samples_to_filter = prepare_sample_list(elements = samples_to_filter, sample_names = sample_names, sample_ordering=sample_ordering)
-  return df.drop(samples_to_filter)
-
-def check_labels(sample_list, sample_label_dict, sample_ordering = None):
-  for i, sample in enumerate(prepare_sample_list(elements = sample_list, sample_ordering=sample_ordering)):
-    if sample_ordering is not None:
-      print(str(np.where(sample_ordering == sample)[0]) +', '+sample +': '+labels_1[sample])
-    else:
-      print(sample +': '+sample_label_dict[sample])
 
